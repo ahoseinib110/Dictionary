593a185ba64fbf2514a58e90111c50e97cb746ea
seyed ali <ahoseinib110@gmail.com>
2020-08-28 09:34:15 +0430

    adding DicFragment in DicActivity

--- app/src/main/java/org/maktab/dictionary/controller/activity/DicActivity.java
+++ app/src/main/java/org/maktab/dictionary/controller/activity/DicActivity.java
@@ -1,15 +1,28 @@
 package org.maktab.dictionary.controller.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 import org.maktab.dictionary.R;
+import org.maktab.dictionary.controller.fragment.DicFragment;
 
 public class DicActivity extends AppCompatActivity {
+    DicFragment mDicFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dic);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mDicFragment = (DicFragment) fragmentManager.findFragmentById(R.id.fragment_container);
+        if(mDicFragment==null){
+            mDicFragment = DicFragment.newInstance();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container,mDicFragment)
+                    .commit();
+        }
     }
+
 }
